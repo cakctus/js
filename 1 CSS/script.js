330834d93f7dbed34433aa9adf387be2e77c819d
Cakctus <cakctusinc@gmail.com>
2023-05-06 09:38:21 +0300

    commit

--- 1 CSS/script.js
+++ 1 CSS/script.js
@@ -1,19 +1,18 @@
-let content = document.getElementsByClassName('section_content');
+let content = document.getElementsByClassName("section_content")
 
 for (i = 0; i < content.length; i++) {
-    content[i].addEventListener("click", toggleItem, false);
+  content[i].addEventListener("click", toggleItem, false)
 }
 
 function toggleItem() {
-    var itemClass = this.classList;
-    for (i = 0; i < content.length; i++) {
-        content[i].className = 'section_content';
-    }
-    if (itemClass == 'section_content') {
-        this.className = 'section_content_open';
-    } else if (this.className == 'section_content_open') {
-        this.className = 'section_content '
-    }
+  var itemClass = this.classList
+  const a = "somevar"
+  for (i = 0; i < content.length; i++) {
+    content[i].className = "section_content"
+  }
+  if (itemClass == "section_content") {
+    this.className = "section_content_open"
+  } else if (this.className == "section_content_open") {
+    this.className = "section_content "
+  }
 }
-
-
